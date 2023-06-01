30ccaa1637b9ba788ebc1473021e89577d10e872
yoshiya12x <yoppie.bull@gmail.com>
2023-06-01 22:52:15 +0900

    Initial Commit

---
+++ SwipeAnimation/SwipeAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeAnimationApp.swift
+//  SwipeAnimation
+//
+//  Created by 前田欣耶 on 2023/06/01.
+//
+
+import SwiftUI
+
+@main
+struct SwipeAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
